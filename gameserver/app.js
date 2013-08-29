5f12f9004c2e143b35bf2c41c67ba719f1af1195
unknown <jonbru@KIWI.isotop.ad>
2013-08-29 14:19:54 +0200

    Added nodejs server

---
+++ gameserver/app.js
@@ -0,0 +1,39 @@
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+// Start listening on port 8080
+server.listen(8080);
+
+// Serve the index.html file
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+// Called when client connects
+var players = new Array();
+io.sockets.on('connection', function (client) {
+  // Called when receving 'message' from the client
+  client.on('connectplayer', function (data) {
+    // Log data to the console
+    players.push(data.playername);
+    console.log(data);
+    // Sends a message to all connected clients
+    io.sockets.emit('connectplayer', players);
+  });
+  client.on('message', function (data) {
+    // Log data to the console
+    console.log(data);
+    // Sends a message to all connected clients
+    io.sockets.emit('message', data);
+  });
+});
+
+/*io.sockets.on('events', function (client) {
+  client.on('click', function (data) {
+    // Log data to the console
+    console.log(data);
+    // Sends a message to all connected clients
+    io.sockets.emit('click', data);
+  });
+});*/
