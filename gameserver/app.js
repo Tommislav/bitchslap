ba2c16de8bb3dc417e5c3c35a5af85e28145bdf5
unknown <jonbru@KIWI.isotop.ad>
2013-08-29 16:35:38 +0200

    server with external settings js file and recieve players

--- gameserver/app.js
+++ gameserver/app.js
@@ -13,19 +13,24 @@ app.get('/', function (req, res) {
 app.get('/mainboard', function (req, res) {
     res.sendfile(__dirname + '/mainboard.html');
 });
+app.get('/settings.js', function (req, res) {
+    res.sendfile(__dirname + '/settings.js');
+});
 
+
+var colors = ["#ff0000", "#00ff00", "#00ff00", "#cccccc", "#ffccff"];
 
 // Called when client connects
-//var players = new Array();
+var players = new Array();
 io.sockets.on('connection', function (client) {
   // Called when receving 'message' from the client
-  client.on('message', function (data) {
+  client.on('connectPlayer', function (id) {
     // Log data to the console
     //players.push(data.playername);
-    console.log(data);
+    players.push('{ id: '+id+', name: "'+colors[players.length]+'", color: "'+colors[players.length]+'", alive: true  }');
+    console.log(id);
+    
     // Sends a message to all connected clients
-    io.sockets.emit('message', data);
-
-
+    io.sockets.emit('playerStatus', players);
   });
 });
